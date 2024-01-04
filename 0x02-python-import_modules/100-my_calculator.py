d73795f16d84018645e3982a44811dbf3c281328
Mamba-cita <EDWINSJOHN52@gmail.com>
2024-01-04 10:20:05 -0800

    fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
-    from magic_calculation_102 import add, sub
-    if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return (c)
+from sys import argv
+from calculator_1 import add, sub, mul, div
+if __name__ == '__main__':
+    if len(argv) != 4:
+        print(f"Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    operator = argv[2]
+
+    if operator == '+':
+        print("{} {} {} = {}".format(a, operator, b, add(a, b)))
+    elif operator == '-':
+        print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
+    elif operator == '*':
+        print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
+    elif operator == '/':
+        print("{} {} {} = {}".format(a, operator, b, div(a, b)))
     else:
-        return(sub(a, b))
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
