ff8804336d3844cc3358be42eaa95e79d833a20e
Edwin's <94281852+Mamba-cita@users.noreply.github.com>
2024-02-06 21:02:52 +0300

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
+"""
+Represents a student with first name, last name, and age.
+"""
 
 
 class Student:
-    ''' Student class '''
+    """
+    Represents a student with first name, last name, and age.
+    """
 
     def __init__(self, first_name, last_name, age):
-        ''' Constructor method '''
+        """
+        Initializes a Student object with the given attributes.
+
+        Args:
+            first_name (str): The student's first name.
+            last_name (str): The student's last name.
+            age (int): The student's age.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        ''' Method that returns directory description with filter '''
-
-        if isinstance(attrs, list) and all(isinstance(attr, str)
-                                           for attr in attrs):
-            res = {}
-            for i in attrs:
-                if i in self.__dict__:
-                    res[i] = self.__dict__[i]
-            return res
-        return self.__dict__
+        """Get a dictionary representation of the student"""
+        if attrs is not None:
+            return {attr: getattr(self, attr)
+                    for attr in attrs if hasattr(self, attr)}
+        else:
+            return self.__dict__
