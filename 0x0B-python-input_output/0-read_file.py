d978ef6b9a1fa39076ca1fa61597279ec938e663
Mamba-cita <EDWINSJOHN52@gmail.com>
2024-02-06 09:48:33 -0800

    initial

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Function reads a file and prints to the stdout"""
+
+
+def read_file(filename=""):
+    """Reads the contents of a UTF-8 text file and prints it to stdout.
+
+    Args:
+        filename (str, optional): The name of the file to read. Defaults to "".
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        file_content = file.read()
+        print(file_content, end='')
