c9feeceb3207a875196fcaf0eaf9afa4988779ce
Edwin's <94281852+Mamba-cita@users.noreply.github.com>
2024-03-13 20:12:16 +0000

    initial

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,11 @@
+-- Lists all shows without the comedy genre in the database hbtn_0d_tvshows.
+-- Records are ordered by ascending show title.
+SELECT title
+FROM tv_shows
+WHERE title NOT IN
+        (SELECT title
+         FROM tv_shows
+         INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+         INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+         WHERE tv_genres.name = 'Comedy' )
+ORDER BY title;
---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,9 @@
+-- Lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+-- Records are ordered by descending rating.
+SELECT tv_genres.name AS name,
+       SUM(tv_show_ratings.rate) AS rating
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY name
+ORDER BY rating DESC;
