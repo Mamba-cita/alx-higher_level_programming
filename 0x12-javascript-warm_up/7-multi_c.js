de6b48602adbc5206bef46757be9f702cfa699b0
Mamba-cita <EDWINSJOHN52@gmail.com>
2024-04-08 15:33:52 +0300

    initial

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const args = process.argv[2];
+let number = parseInt(args);
+
+if (isNaN(number)) {
+  console.log('Missing number of occurrences');
+}
+
+while (number > 0) {
+  console.log('C is fun');
+  number--;
+}
