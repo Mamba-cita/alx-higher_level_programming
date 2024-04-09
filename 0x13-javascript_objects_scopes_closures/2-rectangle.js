a09a891aa8621b470e0e1400bc42a4a0de9c2c16
Mamba-cita <EDWINSJOHN52@gmail.com>
2024-04-09 20:23:51 +0300

    initial-3

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor (w, h) {
-      if (w >= 1 && h >= 1) {
-        this.width = w;
-        this.height = h;
-      }
+  constructor (w, h) {
+    if (w >= 1 && h >= 1) {
+      this.width = w;
+      this.height = h;
     }
   }
-  
-  module.exports = Rectangle;
+}
+
+module.exports = Rectangle;
+
   
