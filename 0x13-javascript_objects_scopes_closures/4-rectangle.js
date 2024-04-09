e35bc706546c096b25b0b1243133ac1a508c4147
Mamba-cita <EDWINSJOHN52@gmail.com>
2024-04-09 20:27:22 +0300

    initial-4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,29 +1,27 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
+const Rectangle = class {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
     }
-  
-    print () {
-      let s = this.height;
-      while (s) {
-        console.log('X'.repeat(this.width));
-        s--;
-      }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
-  
-    double () {
-      this.width *= 2;
-      this.height *= 2;
-    }
-  
-    rotate () {
-      const change = this.width;
-      this.width = this.height;
-      this.height = change;
-    }
-  };
-  
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
+module.exports = Rectangle;
