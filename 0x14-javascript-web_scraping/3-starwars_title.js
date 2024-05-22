ef125ba58a6b5eacb951189c51509a4af4fa5562
Mamba-cita <EDWINSJOHN52@gmail.com>
2024-05-22 18:36:14 +0000

    fix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,12 @@
-t request = require('request');
+#!/usr/bin/node
+
+// JScript uses API to get Starwars info
+
+const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
 request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
+  if (error) { console.log(error); } else {
     const content = JSON.parse(body);
     console.log(content.title);
   }
