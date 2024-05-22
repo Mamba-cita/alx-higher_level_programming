189e86a56fa704684826a94f0d6a89cbb4681f7f
Mamba-cita <EDWINSJOHN52@gmail.com>
2024-05-22 18:01:54 +0000

    initial

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,9 @@
+t filesys = require('fs');
+filesys.readFile(process.argv[2], 'utf-8',
+  function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(data);
+  });
